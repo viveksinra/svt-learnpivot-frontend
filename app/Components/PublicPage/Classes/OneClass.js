493573d4fdc290dd36669f35617a18580cc6165d
viveksinra <vivek.kr.vks@gmail.com>
2025-02-26 13:03:36 +0530

    refactor: enhance FilterDialog with animated buttons and clear filters functionality

--- app/Components/PublicPage/Classes/OneClass.js
+++ app/Components/PublicPage/Classes/OneClass.js
@@ -222,7 +222,7 @@ const OneClass = ({ data }) => {
               hovercolor="#DDD6FE"
               textcolor="#5B21B6"
             >
-              COURSE INFO
+              INFO
             </AnimatedButton>
             <Link href={"/course/buy/" + data._id} style={{ textDecoration: 'none' }}>
               <AnimatedButton>
