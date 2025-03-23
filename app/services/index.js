f4bddebc97fdafa067023eb63f19b1fd0c811cd3
viveksinra <vivek.kr.vks@gmail.com>
2025-03-23 23:37:36 +0530

    refactor: remove multiple service files to streamline codebase and improve maintainability

--- app/services/index.js
+++ app/services/index.js
@@ -1,42 +1,24 @@
 import { AuthService } from "./auth.service";
 import { DashboardService } from "./dashboard.service";
-import { CustomerService } from "./customer.service";
 import { MyCourseService } from "./myCourse.service";
 import { MockTestService } from "./mockTest.service";
 import { RegistrationService } from "./registration.service.js";
 
 
-import { EmployeeService } from "./employee.service";
-import { ResidentService } from "./resident.service";
-import { MedicationService } from "./medication.service";
-import { CareService } from "./care.service";
-import { LedgerService } from "./ledger.service";
-import { PayReceiveService } from "./payReceive.service";
-import { InvoiceService } from "./invoice.service";
-import { AccessService } from "./access.service";
-
 import { API_ENDPOINT } from "../utils/index";
 import { ChildService } from "./child.service";
 import { ReportService } from "./report.service";
 import { MyProfileService } from "./myProfile.service";
+import { TransactionService } from "./transaction.service";
 
 export const authService = new AuthService(API_ENDPOINT);
 export const dashboardService = new DashboardService(API_ENDPOINT);
-export const customerService = new CustomerService(API_ENDPOINT);
 export const myCourseService = new MyCourseService(API_ENDPOINT);
 export const mockTestService = new MockTestService(API_ENDPOINT);
 export const registrationService = new RegistrationService(API_ENDPOINT);
 export const childService = new ChildService(API_ENDPOINT);
 export const myProfileService = new MyProfileService(API_ENDPOINT);
 export const reportService = new ReportService(API_ENDPOINT);
+export const transactionService = new TransactionService(API_ENDPOINT);
 
 
-export const prospectService = new EmployeeService(API_ENDPOINT);
-export const employeeService = new EmployeeService(API_ENDPOINT);
-export const residentService = new ResidentService(API_ENDPOINT);
-export const medicationService = new MedicationService(API_ENDPOINT);
-export const careService = new CareService(API_ENDPOINT);
-export const ledgerService = new LedgerService(API_ENDPOINT);
-export const payReceiveService = new PayReceiveService(API_ENDPOINT);
-export const invoiceService = new InvoiceService(API_ENDPOINT);
-export const accessService = new AccessService(API_ENDPOINT);
---
+++ app/services/transaction.service.js
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { getAuthorizationHeader } from "../utils/getAuthorizationHeader";
+import qs from "qs";
+
+export class TransactionService {
+  instance;
+  constructor(url) {
+    this.instance = axios.create({
+      baseURL: url,
+      // timeout: 30000,
+      timeoutErrorMessage: "Time out!",
+    });
+  }
+
+  publicGetAll = async ( data) => {
+    return this.instance
+      .post(`/api/v1/publicMaster/mockTest/getMockTest/forPublicPage`, data, {
+        
+      })
+      .then((res) => res.data);
+  };
+
+  publicGetOne = async (id) => {
+    return this.instance
+      .get(`/api/v1/publicMaster/mockTest/getMockTest/publicGetOne/${id}`, {
+      })
+      .then((res) => res.data);
+  };
+
+  getMockWithFilter = async ( data) => {
+    return this.instance
+      .post(`/api/v1/publicMaster/otherApi/getRegistration/withFilter`, data, {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+
+  getBuyCourseWithFilter = async ( data) => {
+    return this.instance
+      .post(`/api/v1/publicMaster/otherApi/getCourse/withFilter`, data, {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+  getMockTestNumbersApi = async ( ) => {
+    return this.instance
+      .get(`/api/v1/publicMaster/report/admin/getMockTestNumbers`,  {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+  getCourseTestNumbersApi = async ( ) => {
+    return this.instance
+      .get(`/api/v1/publicMaster/report/admin/getCourseTestNumbers`,  {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+  getCourseParentNumbersApi = async ( ) => {
+    return this.instance
+      .get(`/api/v1/publicMaster/report/admin/getCourseParentNumbers`,  {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+  getCourseDropDown = async ( ) => {
+    return this.instance
+      .get(`/api/v1/publicMaster/course/getCourse/forDropDown`,  {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+
+
+  getAllUser = async ( ) => {
+    return this.instance
+      .get(`/api/v1/publicMaster/report/user/getAllUser`,  {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+  getAllChild = async ( ) => {
+    return this.instance
+      .get(`/api/v1/publicMaster/report/user/getAllChild`,  {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+  getAllChildForDropDown = async ( ) => {
+    return this.instance
+      .get(`/api/v1/publicMaster/otherApi/admin/getAllChildForDropDown`,  {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+  
+  getOneUserOrChildReport = async ( data) => {
+    return this.instance
+      .post(`/api/v1/publicMaster/otherApi/admin/getOneUserOrChildReport`, data, {
+        headers: getAuthorizationHeader(),
+      })
+      .then((res) => res.data);
+  };
+  
+
+}
