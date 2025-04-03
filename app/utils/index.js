a8cbeba22db07b6c90cfe46c11c476c33c864d4d
viveksinra <vivek.kr.vks@gmail.com>
2025-04-03 16:22:59 +0530

    feat: update API endpoint configuration in utils; switch to production API for SVT and comment out local development endpoint

--- app/utils/index.js
+++ app/utils/index.js
@@ -3,12 +3,12 @@
 // export const API_ENDPOINT = "https://api.chelmsford11plus.com";
 // export const FRONT_ENDPOINT = "https://chelmsford11plus.com";
 
-export const API_ENDPOINT = "http://localhost:2040";
+// export const API_ENDPOINT = "http://localhost:2040";
 // export const FRONT_ENDPOINT = "http://localhost:3000";
 
 
 // SVT
-// export const API_ENDPOINT = "https://svtapi.chelmsford11plus.com";
+export const API_ENDPOINT = "https://svtapi.chelmsford11plus.com";
 export const FRONT_ENDPOINT = "https://svt.chelmsford11plus.com";
 
 // export const API_ENDPOINT = "http://localhost:2040";
