1f62e4abec5dea3a64cd83477e4c5e3a74d7fae5
viveksinra <vivek.kr.vks@gmail.com>
2025-05-02 12:05:22 +0530

    fix: uncomment production API endpoints and comment out local development endpoints in utils

--- app/utils/index.js
+++ app/utils/index.js
@@ -8,9 +8,9 @@
 
 
 // SVT
-// export const API_ENDPOINT = "https://svtapi.chelmsford11plus.com";
-// export const FRONT_ENDPOINT = "https://svt.chelmsford11plus.com";
+export const API_ENDPOINT = "https://svtapi.chelmsford11plus.com";
+export const FRONT_ENDPOINT = "https://svt.chelmsford11plus.com";
 
-export const API_ENDPOINT = "http://localhost:2040";
-export const FRONT_ENDPOINT = "http://localhost:3000";
+// export const API_ENDPOINT = "http://localhost:2040";
+// export const FRONT_ENDPOINT = "http://localhost:3000";
 
