1b7ecf5ef9d2d124385384c083b280a759b02349
viveksinra <vivek.kr.vks@gmail.com>
2025-06-24 15:16:35 +0530

    feat: update Stripe keys in frontKey.js to include new keys for 'apat-education' and 'priya-education', enhancing payment processing capabilities

--- app/utils/frontKey.js
+++ app/utils/frontKey.js
@@ -1,23 +1,34 @@
 module.exports = {
 
-    stripeKeys: [
-     {
-       id:"key1",
-       label:"default-navin",
-       LivePublishablekey:"pk_live_51OutBL02jxqBr0evcB8JFdfck1DrMljCBL9QaAU2Qai5h3IUdGgh22m3DCu1VMmWvn4tqEFcFdwfT34l0xh8e28s00YTdA2C87",
-        
-       TestPublishablekey:"pk_test_51OutBL02jxqBr0ev5h0jPo7PWCsg0z3dDaAtKPF3fm8flUipuFtX7GFTWO2eLwVe6JzsJOZJ0f2tQ392tCgDWwdt00iCW9Qo66",
-     },
-     {
-       // not updated
-       id:"key2",
-       label:"apat-education",
-       LivePublishablekey:"pk_live_51OutBL02jxqBr0evcB8JFdfck1DrMljCBL9QaAU2Qai5h3IUdGgh22m3DCu1VMmWvn4tqEFcFdwfT34l0xh8e28s00YTdA2C87",
-       
-       TestPublishablekey:"pk_test_51OutBL02jxqBr0ev5h0jPo7PWCsg0z3dDaAtKPF3fm8flUipuFtX7GFTWO2eLwVe6JzsJOZJ0f2tQ392tCgDWwdt00iCW9Qo66",
-     },
- 
-    ],
- 
- 
- };
+  stripeKeys: [
+   {
+     id:"key1",
+     label:"default-navin",
+     LivePublishablekey:"pk_live_51OutBL02jxqBr0evcB8JFdfck1DrMljCBL9QaAU2Qai5h3IUdGgh22m3DCu1VMmWvn4tqEFcFdwfT34l0xh8e28s00YTdA2C87",
+
+     
+     TestPublishablekey:"pk_test_51OutBL02jxqBr0ev5h0jPo7PWCsg0z3dDaAtKPF3fm8flUipuFtX7GFTWO2eLwVe6JzsJOZJ0f2tQ392tCgDWwdt00iCW9Qo66",
+   },
+   {
+     //  updated
+     id:"key2",
+     label:"apat-education",
+     LivePublishablekey:"pk_live_51RZ8Pw07lDsmwlCKuzP73RmkqP9bmY1IB2UeIRxfb39amBXXQhwTntixGcfDr2G4bAtWscr6VIS9TQzmLA9sUa39001828gaVz",
+
+     
+     TestPublishablekey:"pk_test_51RZ8Pw07lDsmwlCKiDbckMrMhOf6Fe2iXRA73bcPLQEYLxGvw7ejdemtkwGq865n4fuPP5nNgf7CNhhWwgCTRBV900sxyEyGL4",
+   },
+   {
+     //  updated
+     id:"key3",
+     label:"priya-education",
+     LivePublishablekey:"pk_live_51RZ7nCCNfnbw7j8w7CnHEYCp7bxuqUutAVxuypcc7A4KMaf54YgeaICop0FMCIfVNwg7a1MW37J7MLeJHttfGT3B00PKPV1N5x",
+
+     
+     TestPublishablekey:"pk_test_51RZ7nCCNfnbw7j8wBJM49QfC52YWRmwDRBCjb8gCORSFo9U6R03zVFhRDoBaMVHviNZciUKP5Vi3kT7B0WTPQosk003b7UBCHM",
+   },
+
+  ],
+
+
+};
